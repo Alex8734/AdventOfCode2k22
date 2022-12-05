291795db7611df85aa2b529bb687cc546550d2f3
Alexander Resch <a.resch@students.htl-leonding.ac.at>
2022-12-05 08:09:20 +0100

    part1 works

---
+++ Day4/Program.cs
@@ -0,0 +1,30 @@
+﻿//part1
+var file = File.ReadAllLines("data.csv");
+int count = 0;
+foreach (var line in file)
+{
+    var pairs = line.Split(',');
+    var pair1 = new Rooms(pairs[0]);
+    var pair2 = new Rooms(pairs[1]);
+
+    if ((pair1.Start >= pair2.Start && pair1.End <= pair2.End)
+        || (pair1.Start <= pair2.Start && pair1.End >= pair2.End))
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
+
+class Rooms
+{ 
+    public int Start { get; }
+    public int End { get; }
+
+    public Rooms(string startEnd)
+    {
+        var data = startEnd.Split("-");
+        Start = int.Parse(data[0]);
+        End = int.Parse(data[1]);
+    }
+}
