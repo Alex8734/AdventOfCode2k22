b63a77342f613c1a3f6a54a2b7d05f736a4964e2
Alexander Resch <a.resch@students.htl-leonding.ac.at>
2022-12-05 08:39:22 +0100

    part2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -16,6 +16,22 @@ foreach (var line in file)
 
 Console.WriteLine(count);
 
+//part2
+count = 0;
+foreach (var line in file)
+{
+    var pairs = line.Split(',');
+    var pair1 = new Rooms(pairs[0]);
+    var pair2 = new Rooms(pairs[1]);
+
+    if (pair1.IsOverlapping(pair2))
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
+
 class Rooms
 { 
     public int Start { get; }
@@ -26,5 +42,11 @@ class Rooms
         var data = startEnd.Split("-");
         Start = int.Parse(data[0]);
         End = int.Parse(data[1]);
+    }
+
+    public bool IsOverlapping(Rooms rooms2)
+    {
+        return !(rooms2.Start > End ||
+                 rooms2.End < Start);
     }
 }
